15db8580ee5a9e723943280706b726f1e35dd6a5
erymski <erymski@gmail.com>
2016-11-12 15:40:45 +0300

    Split landing page for admin and user after log in.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,11 @@ class StaticController < ApplicationController
 
   def index
     if user_signed_in? then
-      redirect_to url_for(:controller => 'image_label_sets', :action=>'index')
+      if current_user.is_admin then
+        redirect_to url_for(:controller => 'admin', :action => 'all')
+      else
+        redirect_to url_for(:controller => 'image_label_sets', :action => 'index')
+      end
     end 
   end
   
